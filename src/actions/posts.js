d0fe06f795da44412360c2a082634d34f198a996
musekwa <musekwa2011@gmail.com>
2021-08-18 05:49:18 +0200

    Memory app client setup

---
+++ src/actions/posts.js
@@ -0,0 +1,22 @@
+import * as api from '../api';
+
+// Action Creators
+export const getPosts = () => async (dispatch) =>{
+    try {
+        const { data } = await api.fetchPosts();
+        dispatch({ type: 'FETCH_ALL', payload: data });
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+
+export const createPost = (newPost) => async (dispatch) =>{
+    try {
+        const { data } = await api.createPost(newPost);
+        dispatch({ type: 'CREATE', payload: data })
+    }
+    catch(error) {
+        console.log(error.message)
+    }
+}
